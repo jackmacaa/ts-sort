df5cd3b872a67bc525e58ca87ce7785884e6971e
jack.mcloughlin <jackmaca@msn.com>
2023-01-12 11:44:37 +1030

    finalised with abstract classes

--- src/CharactersCollection.ts
+++ src/CharactersCollection.ts
@@ -1,5 +1,9 @@
-export class CharactersCollection {
-  constructor(public data: string) {}
+import { Sorter } from './Sorter';
+
+export class CharactersCollection extends Sorter {
+  constructor(public data: string) {
+    super();
+  }
 
   get length(): number {
     return this.data.length;
@@ -7,17 +11,16 @@ export class CharactersCollection {
 
   compare(leftIndex: number, rightIndex: number): boolean {
     return (
-      this.data[leftIndex].toLowerCase() >
-      this.data[rightIndex].toLocaleLowerCase()
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
     );
   }
 
   swap(leftIndex: number, rightIndex: number): void {
     const characters = this.data.split('');
 
-    const lefthand = characters[leftIndex];
+    const leftHand = characters[leftIndex];
     characters[leftIndex] = characters[rightIndex];
-    characters[rightIndex] = lefthand;
+    characters[rightIndex] = leftHand;
 
     this.data = characters.join('');
   }
--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,10 +1,14 @@
-export class NumbersCollection {
-  constructor(public data: number[]) {}
+import { Sorter } from './Sorter';
+
+export class NumbersCollection extends Sorter {
+  constructor(public data: number[]) {
+    super();
+  }
 
   get length(): number {
     return this.data.length;
   }
-  
+
   compare(leftIndex: number, rightIndex: number): boolean {
     return this.data[leftIndex] > this.data[rightIndex];
   }
